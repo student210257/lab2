ecf9aef5e6fb6fd25a0bb6262abab67c17fa9f7c
Paweł Ksieniewicz <pawel@ksienie.com>
2014-03-02 10:57:35 +0100

    Clean-up.

--- main.cc
+++ main.cc
@@ -2,11 +2,6 @@
 #include <math.h>
 #include "Array.hh"
 
-using namespace std;
-
-#pragma mark -
-#pragma mark Prototypy
-
 float circle_circumference(float radius);
 float circle_area(float radius);
 
@@ -20,9 +15,6 @@ int main()
 	printf("Circle circumference is %.2f.\n", circle_circumference(circle_radius));
 	printf("Circle area is %.2f.\n", circle_area(circle_radius));
 }
-
-#pragma mark -
-#pragma mark Funkcje
 
 float circle_circumference(float radius)
 {
